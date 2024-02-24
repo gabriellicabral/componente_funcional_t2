c9dd5fdfb003a1b071f3dc0fdb8e0a02fa6a2f83
gabriellicabral <gabrielli.cabral2017@gmail.com>
2024-02-23 21:04:31 -0300

    criando um componente funcional

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Header from "./componentes/Header/Header.jsx"
+import "./App.css"
+export default function App(){
+  return(
+    <Header/>
+  )
+}
---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+export default function Header (){
+    return(
+        <header>
+            
+                <h1>Boa tardeee! Somente para as instrutoras mais queridas no VnW!</h1>
+           
+        </header>
+
+    )
+}
